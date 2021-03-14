d676b7ae9c77708f092f6eb6ccee36011db1541d
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-03-14 22:22:30 +0530

    button package added

---
+++ packages/button/src/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mgr8-Button">
+    <button>
+      <slot />
+    </button>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Mgr8Button",
+  props: {},
+  setup() {
+    // your code
+  },
+});
+</script>
