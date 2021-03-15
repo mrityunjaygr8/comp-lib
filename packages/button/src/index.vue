1eaa8d79d2dfd28225c0387f165115878465e578
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-03-15 22:52:24 +0530

    type and block props added to button comp; variants defined

--- packages/button/src/index.vue
+++ packages/button/src/index.vue
@@ -1,17 +1,83 @@
 <template>
-  <div class="mgr8-Button">
+  <div
+    class="mgr8-Button"
+    :class="type"
+    :style="{ display: display, width: width }"
+  >
     <button>
       <slot />
     </button>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, Ref, ref } from "vue";
 export default defineComponent({
   name: "Mgr8Button",
-  props: {},
-  setup() {
-    // your code
+  props: {
+    type: {
+      type: String,
+      default: "primary",
+      validator: (value: string) =>
+        ["primary", "plain", "full"].includes(value),
+    },
+    block: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    let display: Ref<string> = ref("inline-block");
+    let width: Ref<string> = ref("unset");
+
+    if (props.block) {
+      display.value = "block";
+      width.value = "100%";
+    }
+
+    return {
+      display,
+      width,
+    };
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.mgr8-Button {
+  cursor: pointer;
+  text-align: center;
+  line-height: var(--font-base-size);
+  font-weight: 400;
+  padding: 10px 40px;
+  text-transform: uppercase;
+
+  transition: all 0.2s ease-out;
+}
+
+.mgr8-Button.primary {
+  color: var(--text-color);
+  border: dashed 1px var(--text-color);
+  background: transparent;
+
+  &:hover {
+    color: var(--text-color);
+    border-style: solid;
+    background-color: var(--act-color);
+  }
+}
+.mgr8-Button.full {
+  color: var(--bkg-color);
+  border-color: var(--text-color);
+  border: dashed 1px;
+  background-color: var(--text-color);
+
+  &:hover {
+    color: var(--text-color);
+    border-color: var(--text-color);
+    border-style: solid;
+    background-color: var(--act-color);
+  }
+}
+.mgr8-Button.plain {
+}
+</style>
