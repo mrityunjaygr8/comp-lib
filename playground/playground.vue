23096bcb1b6a818e6056f266395856c539a1ff4b
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-03-15 22:51:56 +0530

    test buttons added in playground

--- playground/playground.vue
+++ playground/playground.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="playground" data-theme="default">
     Hello World
-    <mgr8-button>Button asd</mgr8-button>
+    <mgr8-button type="primary">Primary</mgr8-button>
+    <mgr8-button type="plain">Plain</mgr8-button>
+    <mgr8-button type="full">Full</mgr8-button>
+    <mgr8-button block>Block Primary</mgr8-button>
+    <mgr8-button>Default</mgr8-button>
   </div>
 </template>
 
